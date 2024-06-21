e975a994440e33299fb0108d35b2157f00ea02df
pawanharry <wazirpawandeep23@gmail.com>
2024-06-21 01:21:00 -0400

    Updations in navbar and banner and footer

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Navbar = () => {
+const [sticky,setStickey]=useState(false)
+useEffect(()=>{
+  const handleScroll=()=>{
+    if(window.scrollY>0){
+      setStickey(true)
+    }
+    else {
+      setStickey(true)
+
+    }
+  }
+  window.addEventListener('scroll',handleScroll)
+  return () =>{
+    window.removeEventListener('scroll',handleScroll)
+  }
+
+},[])
+
 const navItems = (
     <>
      <li><a>Home</a></li>
@@ -13,8 +31,11 @@ const navItems = (
 
   return (
     <>
-    <div className='max-w-screen-2xl container mx-auto md:px-20 px-4'>
-    <div className="navbar bg-base-100">
+    <div className={`max-w-screen-2xl container mx-auto md:px-20 px-4 fixed top-0 left-0 right-0 ${
+      sticky?"sticky-navbar shadow-md bg-base-200 duration-300 transition-all ease-in-out":""}
+    `}
+    >
+    <div className="navbar">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">

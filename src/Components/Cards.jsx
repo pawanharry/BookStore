1b60c789542c090bf304f4b339e5b1b25131931c
pawanharry <wazirpawandeep23@gmail.com>
2024-06-27 01:55:08 -0400

    themeChanger

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -5,7 +5,7 @@ function Cards({item}) {
   return (
     <>
     <div className='mt4 my-3 p-3' >
-    <div className="card w-92  bg-base-100 shadow-xl hover:scale-105 duration-200">
+    <div className="card w-92  bg-base-100 shadow-xl hover:scale-105 duration-200 dark:bg-slate-900 dark:text-white dark:border">
   <figure>
     <img src={item.image} alt="books" /></figure>
   <div className="card-body">

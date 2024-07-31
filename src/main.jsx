f7f42a7aadb6be7220aaaa3d916c5df97304d519
pawanharry <wazirpawandeep23@gmail.com>
2024-07-31 16:50:36 -0400

    com

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+<<<<<<< Updated upstream
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+=======
+import { HashRouter  } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <HashRouter>
+    <div className='dark:bg-slate-900 dark:text-white'>
+       <App />
+
+    </div>
+    
+  </HashRouter>
+  
+);
+>>>>>>> Stashed changes
